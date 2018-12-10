7f58f0d3f60258f505806c0ff5768e9a89983ed8
HectorV12 <Hector_Vazquez@live.com>
2018-12-10 16:59:04 -0500

    ProductDetails Design Demo (Unfinished)

--- lib/product_details_screen/product_details_screen.dart
+++ lib/product_details_screen/product_details_screen.dart
@@ -9,19 +9,24 @@ class ProductDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0.0,
-        leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: () {
-              hasScanned = false;
-              qrCameraKey.currentState.restart();
-              Navigator.pop(context);
-            }),
-      ),
-      body: Center(
-        child: FutureBuilder<Product>(
+    return WillPopScope(
+      onWillPop: () async {
+        hasScanned = false;
+        qrCameraKey.currentState.restart();
+        Navigator.pop(context);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0.0,
+          leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+                hasScanned = false;
+                qrCameraKey.currentState.restart();
+                Navigator.pop(context);
+              }),
+        ),
+        body: FutureBuilder<Product>(
           future: fetchProduct(productCode),
           builder: (context, snapshot) {
             switch (snapshot.connectionState) {
@@ -30,21 +35,101 @@ class ProductDetails extends StatelessWidget {
                 return new Center(child: new CircularProgressIndicator());
               default:
                 if (snapshot.hasData) {
-                  return Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                  return ListView(
                     children: <Widget>[
-                      snapshot.data.imageUrl == null
-                          ? Text('Image Not Found\n')
-                          : Image.network(snapshot.data.imageUrl),
-                      snapshot.data.code == null
-                          ? Text('Product Code Not Found\n')
-                          : Text('Prodcut Barcode#: ' + snapshot.data.code),
-                      snapshot.data.brand == null
-                          ? Text('Product Brand Not Found\n')
-                          : Text('Prodcut Brand: ' + snapshot.data.brand),
-                      snapshot.data.productName == null
-                          ? Text('Product Name Not Found\n')
-                          : Text('Prodcut Name: ' + snapshot.data.productName),
+                      AppBar(
+                        automaticallyImplyLeading: false,
+                        flexibleSpace: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                          child: Column(
+                            children: <Widget>[
+                              Row(
+                                children: <Widget>[
+                                  Center(
+                                    child: snapshot.data.imageUrl == null
+                                        ? Text('Image Not Found\n')
+                                        : Image.network(
+                                            snapshot.data.imageUrl,
+                                            width: 137.0,
+                                            height: 137.0,
+                                          ),
+                                  ),
+                                  Padding(
+                                    padding:
+                                        EdgeInsets.symmetric(horizontal: 5.0),
+                                  ),
+                                  Flexible(
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: <Widget>[
+                                        snapshot.data.brand == null
+                                            ? Text('Product Brand Not Found\n')
+                                            : Text(
+                                                '${snapshot.data.brand}',
+                                                style: TextStyle(
+                                                  fontSize: 20.0,
+                                                  fontWeight: FontWeight.bold,
+                                                  color: Colors.black,
+                                                ),
+                                                textAlign: TextAlign.left,
+                                              ),
+                                        snapshot.data.productName == null
+                                            ? Text('Product Name Not Found\n')
+                                            : Text(
+                                                '${snapshot.data.productName}\n',
+                                                style: TextStyle(
+                                                  fontSize: 20.0,
+                                                  fontWeight: FontWeight.bold,
+                                                  color: Colors.black,
+                                                ),
+                                                textAlign: TextAlign.left,
+                                              ),
+                                        Text('Best Before:'),
+                                        Text('(Date)'),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Divider(),
+                              Padding(
+                                padding: const EdgeInsets.only(bottom: 5.0),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: <Widget>[
+                                    Container(
+                                      width: 40.0,
+                                      height: 40.0,
+                                      color: Colors.grey,
+                                    ),
+                                    Container(
+                                      width: 40.0,
+                                      height: 40.0,
+                                      color: Colors.grey,
+                                    ),
+                                    Container(
+                                      width: 40.0,
+                                      height: 40.0,
+                                      color: Colors.grey,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Card(
+                        margin: EdgeInsets.all(16.0),
+                        child: Center(
+                          heightFactor: 5.0,
+                          child: snapshot.data.code == null
+                              ? Text('Product Code Not Found\n')
+                              : Text('Prodcut Barcode#: ' + snapshot.data.code),
+                        ),
+                      ),
                     ],
                   );
                 } else if (snapshot.hasError) {
